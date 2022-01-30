c7e87e605757d33fa65d5b006c9d1fdf7845676f
Matthijs van Schooten <matthijs@munisense.com>
2022-01-30 12:51:31 +0100

    Converted to work with FactoriGOChatBot

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # FactoriGOChatBot-companion
 A companion mod for the [FactoriGOChatBot](https://github.com/Mattie112/FactoriGOChatBot) that allow for extra things to be send to Discord
 
-- The bot will work without this mod
-- You don't need this mod, a manual edit of the save file is also an option
+- The bot will work without this mod (for chat/join/leave)
+- You don't need this mod, a manual edit of the save file is also an option (see https://github.com/Mattie112/FactoriGOChatBot/blob/main/README.md)
 - This mod logs additional things, it does not change your gameplay in any way
+- Data will be logged into the file `your-factorio-folder/script-output/factorigo-chat-bot/factorigo-chat-bot.log`, this file (or directory) must be accesable to the bot
 
-More details will follow soon.
+Extra data supported:
+- Research started / finished
+- Player deaths
+- Rocket launched
--- control.lua
+++ control.lua
@@ -1,19 +1,21 @@
-local baseFolder = "factorio-chat-bot/";
-local baseFile = "factorio-chat-bot.log"
+local baseFolder = "factorigo-chat-bot/";
+local baseFile = "factorigo-chat-bot.log"
+
+-- Start with cleaning the logfile of a previous run
+game.write_file(baseFolder .. baseFile, "", false)
 
 local function log_message(msg)
     msg = game.tick .. " " .. msg
-    -- todo appending a file (the last parameter) does not seem to be working so atm it will only log the last action
-    game.write_file(baseFolder .. baseFile, "[FactorioChatBot]: " .. serpent.line(msg, { comment = false }, true))
+    game.write_file(baseFolder .. baseFile, "[FactoriGOChatBot]: " .. serpent.line(msg, { comment = false }), true)
 end
 
 script.on_event(defines.events.on_research_finished, function(event)
-    local research_name = event.research.name
+    local research_name = event.research.localised_name
     log_message("[RESEARCH_FINISHED:" .. research_name .. "]")
 end)
 
 script.on_event(defines.events.on_research_started, function(event)
-    local research_name = event.research.name
+    local research_name = event.research.localised_name
     log_message("[RESEARCH_STARTED:" .. research_name .. "]")
 end)
 
